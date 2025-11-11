2b73abf56b6313795e19288a5bd22605960fa476
Pedro Saturno <pedro1saturno@gmail.com>
2025-11-11 01:05:06 -0300

    criação do repositório de Curso

---
+++ src/main/java/com/example/estudospring/repository/CursoRepository.java
@@ -0,0 +1,10 @@
+package com.example.estudospring.repository;
+
+import com.example.estudospring.domain.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+}
