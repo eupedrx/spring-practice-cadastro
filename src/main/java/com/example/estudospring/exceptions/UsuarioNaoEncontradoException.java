ac4f3a0d41c57b32fa49f4c0fd067b36b60f225b
Pedro Saturno <pedro1saturno@gmail.com>
2025-11-11 22:08:50 -0300

    adição de HttpStatus not_found

--- src/main/java/com/example/estudospring/exceptions/UsuarioNaoEncontradoException.java
+++ src/main/java/com/example/estudospring/exceptions/UsuarioNaoEncontradoException.java
@@ -1,5 +1,10 @@
 package com.example.estudospring.exceptions;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UsuarioNaoEncontradoException extends RuntimeException {
     public UsuarioNaoEncontradoException(String message) {
         super(message);
