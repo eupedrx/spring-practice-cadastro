51e72521e64ef8e0c45007e9b6ed46daeeb04740
Pedro Saturno <pedro1saturno@gmail.com>
2025-12-03 00:52:46 -0300

    refatoração no código de User

--- src/main/java/com/example/estudospring/domain/User.java
+++ src/main/java/com/example/estudospring/domain/User.java
@@ -2,26 +2,52 @@ package com.example.estudospring.domain;
 
 
 import jakarta.persistence.*;
-import jdk.jfr.Name;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchConnectionDetails;
 
-import java.math.BigDecimal;
 
-@ToString
-@Getter
-@Setter
 @Entity
 @Table(name = "usuarios")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "nome")
-    private String firstName;
-    @Column(name = "idade")
-    private int age;
+    @Column(name = "nome", nullable = false)
+    private String name;
+    @Column(name = "senha", nullable = false)
+    private String password;
+
+
+    public User() {
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
