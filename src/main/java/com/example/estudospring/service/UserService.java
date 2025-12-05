3c714c9be9a34fb00fde7b249f8d914bf210ae70
Pedro Saturno <pedro1saturno@gmail.com>
2025-12-05 01:55:24 -0300

    adição dos metodos registerUser/findByName e também de um construtor própio devido a criptografia com BCrypt

--- src/main/java/com/example/estudospring/service/UserService.java
+++ src/main/java/com/example/estudospring/service/UserService.java
@@ -4,26 +4,45 @@ import com.example.estudospring.domain.User;
 import com.example.estudospring.exceptions.UsuarioNaoEncontradoException;
 import com.example.estudospring.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-@AllArgsConstructor
+
 public class UserService {
 
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    public User registerUser(String name, String password){
+        String cryptPassword = passwordEncoder.encode(password);
+        User user = new User(name, cryptPassword);
+        userRepository.save(user);
+        return user;
+    }
+
+    public Optional<User> findByName(String name) {
+        return userRepository.findByName(name);
+    }
+
 
     public List<User> listarUsuarios() {
         return userRepository.findAll();
     }
 
     public User listarUsuariosPorId(Long id) {
-
         return userRepository.findById(id)
-        .orElseThrow(() -> new UsuarioNaoEncontradoException("Usuário não encontrado com ID: " + id));
+                .orElseThrow(() -> new UsuarioNaoEncontradoException("Usuário não encontrado com ID: " + id));
     }
 
     public User salvarUsuario(User user) {
@@ -36,4 +55,6 @@ public class UserService {
         }
         userRepository.deleteById(id);
     }
+
+
 }
